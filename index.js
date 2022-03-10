97ba5fca776abf16dd66c2eafa781e197621895f
christierney3 <chris.tierney3@gmail.com>
2022-03-09 23:56:55 -0700

    redid most added libs folder

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const generateHtml = require('./utils/generateHtml')
+const renderTeam = require('./utils/generateHtml');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/intern');
+const Engineer = require('./lib/engineer');
 const inquirer = require('inquirer');
 const fs = require('fs');
-const team = [];
-let newEmployee;
 
-function addManager() {
+let teamObj = {
+    manager: '',
+    interns: [],
+    engineers: [],
+}
+
+function managerQuestions() {
+    let employee = new Employee
     console.log('Build your team.');
     return inquirer.prompt ([
         {
@@ -34,39 +43,40 @@ function addManager() {
     ])
     .then(managerAnswers => {
         const { name, id, email, office } = managerAnswers;
-        const newManager = new Manager(name, id, email, office);
-        team.push(newManager);
+        teamObj.manager = new Manager(name, id, email, office);
+        employeeChoice();
     })
+        // const { name, id, email, office } = managerAnswers;
+        // 
+
+
 };
 
-function addEmployee() {
-    return inquirer.prompt ([
+function engineerQuestions() {
+    inquirer.prompt ([
         {
             type: 'input',
-            message: "What is the employee's name?",
+            message: "What is the engineer's name?",
             name: 'name',
+            default: 'John'
         },
         {
             type: 'input',
-            message: "What is the employee's id #?",
+            message: "What is the engineer's id #?",
             name: 'id',
+            default: '4'
         },
         {
             type: 'input',
-            message: "What is the employee's email?",
+            message: "What is the engineer's email?",
             name: 'email',
+            default: 'john@john.com'
         },
         {
             type: 'input',
             message: "What is the engineer's github username?",
             name: 'github',
-            when: (input) => input.role === 'Engineer',
-        },
-        {
-            type: 'input',
-            message: "Where does your intern go to school?",
-            name: 'school',
-            when: (input) => input.role === 'Intern',
+            default: 'john@github.com',
         },
         {
             type: 'confirm',
@@ -75,32 +85,156 @@ function addEmployee() {
             default: false
         }
     ])
-    .then(employeeAnswers => {
-
-        let { name, id, email, github, school, addMember } = employeeAnswers;
-
-        if (input.role === 'Engineer') {
-        newEmployee = new Employee(name, id, email, github);
-        } else if (input.role === 'Intern') {
-        newEmployee = new Employee(name, id, email, school);
-        }
-
-        team.push(newEmployee);
-
-        if (addMember === true) {
-            return addEmployee();
-        } return team;
+    .then(engineerAnswers => {
+        const { name, id, email, github} = engineerAnswers;
+        teamObj.engineers.push(new Engineer(name, id, email, github));
+        employeeChoice();
     })
 };
+
+function internQuestions() {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            message: "What is the intern's name?",
+            name: 'name',
+            default: 'John'
+        },
+        {
+            type: 'input',
+            message: "What is the intern's id #?",
+            name: 'id',
+            default: '4'
+        },
+        {
+            type: 'input',
+            message: "What is the intern's email?",
+            name: 'email',
+            default: 'john@john.com'
+        },
+        {
+            type: 'input',
+            message: "Where does your intern go to school?",
+            name: 'school',
+            default: 'CU',
+        },
+    ])
+    .then(internAnswers => {
+        const { name, id, email, github} = internAnswers;
+        teamObj.interns.push(Intern(name, id, email, github));
+        employeeChoice();
+    })
+};
+
+function employeeChoice() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'Would you like to add another member to your team?',
+            choices: ['Intern', 'Engineer', 'No']
+        }
+    ])
+    .then((choices) => {
+        const {choice} = choices;
+        if (choice === 'Intern') {
+            internQuestions();
+        } else if (choice === 'Engineer') {
+            engineerQuestions();
+        } else {
+            writeToFile();
+        }
+    })
+}
+
+// function addIntern() {
+//     let intern = new Intern
+//         .then((answers) => {
+//             intern.name = answers.name;
+//             intern.id = answers.id;
+//             intern.email = answers.email;
+//             intern.school = answers.school;
+//             generateHtml.teamCards(intern);
+//         })
+// };
+
+// function addEngineer() {
+
+// };
 
 function writeToFile() {
-    fs.writeFile('./dist/index.html', data, function (err) {
-        if (err) throw err;
+    fs.writeFile('./dist/index.html', renderTeam(teamObj), function(err) {
+        if(err) {
+            throw err;
+        } console.log('Profile generated!');
     })
-    .then(() => console.log('Profile succesfully generated.'))
-    .catch((err) => console.log(err));
+}
+
+function init() {
+    managerQuestions()      
 };
 
-addManager()
-    .then(addEmployee)
-    .then(writeToFile)
+init();
+//     .then(employeeAnswers => {
+
+//         let { name, id, email, github, school, addMember, role } = employeeAnswers;
+
+//         if (role === 'Engineer') {
+//         newEmployee = new Employee(name, id, email, github);
+//         } else if (role === 'Intern') {
+//         newEmployee = new Employee(name, id, email, school);
+//         }
+
+//         team.push(newEmployee);
+
+//         if (addMember === true) {
+//             return addEmployee();
+//         } return team;
+//     })
+// };
+
+// function writeToFile(data) {
+//     fs.writeFile('./dist/index.html', generateHtml(data), function (err) {
+//         err ? console.log(err) : console.log('Profile successfully generated!')
+//     });
+// };
+
+// addManager()
+//     .then(addEmployee)
+//     .then(writeToFile)
+// let {name, id, email, role, github, school} = employeeAnswers;
+//     if (role === 'Engineer') {
+//         let engineer = new Engineer(name, id, email, github)
+//             .then((employeeAnswers) => {
+//                 engineer.name = employeeAnswers.name;
+//                 engineer.id = employeeAnswers.id;
+//                 engineer.email = employeeAnswers.email;
+//                 engineer.github = employeeAnswers.github;
+//                 generateHtml.teamCards(engineer);
+//         })
+//     } else if (role === 'Intern') {
+//         let intern = new Intern(name, id, email, school)
+//             name = employeeAnswers.name;
+//             id = employeeAnswers.id;
+//             email = employeeAnswers.email;
+//             school = employeeAnswers.school;
+//         generateHtml.teamCards(intern);
+//     } else {
+//         generateHtml.generateHtml();
+//     }
+        // .then(employeeQuestions)
+        //     .then((answers) => {
+        //         intern.name = answers.name;
+        //         intern.id = answers.id;
+        //         intern.email = answers.email;
+        //         intern.school = answers.school;
+        //         generateHtml.teamCards(intern);
+        //     })
+
+        //         .then((answers) => {
+        //     manager.name = answers.name;
+        //     manager.id = answers.id;
+        //     manager.email = answers.email;
+        //     manager.office = answers.office;
+        //     generateHtml.teamCards(manager);
+        // })
--- utils/generateHtml.js
+++ utils/generateHtml.js
@@ -1,5 +1,44 @@
-function generateHtml() {
-    return ``
-}
+let allCards = []
+const renderTeam = ({manager, interns, engineers}) => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <main>
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                  <h1 class="display-4">My Team Builder</h1>
+                  <p class="lead">This is a generated team builder using node.js!</p>
+                </div>
+            </div>
+            <div class='card' style='width: 18rem;'>
+                <div class="card-body">
+                    <h5 class="card-title">${manager.getRole()}</h5>
+                    <p class="card-text">${manager.getName()}</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${manager.getId()}</li>
+                    <li class="list-group-item">Email: ${manager.getEmail()}</li>
+                    <li class="list-group-item">Office #: ${manager.office}</li>
+                </ul>
+            </div>
+        </main>
+    </body>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    </html>` 
+};
 
-module.exports = generateHtml;
+
+
+
+
+
+
+module.exports = renderTeam, generateHtml;
